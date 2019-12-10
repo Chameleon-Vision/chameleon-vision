91e10584eca49befc13376c4fb495155c6f0ab96
OmerZ7 <zipory.omer@gmail.com>
2019-12-11 01:12:02 +0200

    Starting values and reorder items

--- chameleon-client/src/views/CameraViewes/StreamTab.vue
+++ chameleon-client/src/views/CameraViewes/StreamTab.vue
@@ -2,10 +2,10 @@
     <div>
         <span>Compression:</span>
         <v-tabs v-model="value.streamCompression" style="padding-bottom:5%" fixed-tabs background-color="#252525" dark height="48" slider-color="#4baf62">
-            <v-tab>None</v-tab>
-            <v-tab>Low</v-tab>
-            <v-tab>Medium</v-tab>
             <v-tab>High</v-tab>
+            <v-tab>Medium</v-tab>
+            <v-tab>Low</v-tab>
+            <v-tab>None</v-tab>
         </v-tabs>
         <span>Frame rate:</span>
         <v-tabs v-model="value.streamFpsMode" style="padding-bottom:5%" fixed-tabs background-color="#252525" dark height="48" slider-color="#4baf62">
