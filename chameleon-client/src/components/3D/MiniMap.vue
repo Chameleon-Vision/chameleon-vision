e608ce203ab8b3f3e43e46e703aa1bcc981258ca
ori agranat <oriagranat9@gmail.com>
2019-12-29 00:33:12 +0200

    re did minimap component

--- chameleon-client/src/components/3D/MiniMap.vue
+++ chameleon-client/src/components/3D/MiniMap.vue
@@ -10,13 +10,12 @@
                 x: 0,
                 y: 0,
             },
-            angle: Number
+            radians: Number
         },
         data() {
             return {
                 ctx: undefined,
                 canvas: undefined,
-                image: undefined
             }
         },
         watch: {
@@ -25,45 +24,53 @@
                 handler() {
                     this.draw();
                 }
-            }
+            },
         },
         methods: {
             draw() {
-                this.ctx.save(); // saving canvas state before setting origin back to norman(0,0)
                 this.clearBoard();
-                this.drawLine();
-                // start of static canvas objects
+                this.drawPlayer();
+                // this.drawLine();
                 this.drawTarget();
                 this.drawText();
-                // end of static canvas objects
-                this.ctx.restore(); // restoring older state but with cleaned canvas
-                this.drawPlayer();
             },
             drawText() {
                 this.ctx.fillStyle = "whitesmoke";
-                this.ctx.fillText(`X: ${this.location.x}, Y: ${this.location.y}, ∠${this.angle}° `, 200 - 15, this.location.y + 25);
+                this.ctx.fillText(`X: ${this.location.x}, Y: ${this.location.y}, ∠${Math.PI * this.radians / 180}° `, this.location.x - 50, this.location.y - 25);
             },
             drawTarget() {
-                this.ctx.fillStyle = "#01a209";
-                this.ctx.fillRect(380, 0, 40, 6); // setting the target rectangle
-            },
-            drawLine() {
-                //player line draw
+                const width = 40;
+                const height = 6;
+                // first save the untranslated/unrotated context
+                this.ctx.save();
                 this.ctx.beginPath();
-                this.ctx.moveTo(400, this.location.y);
-                this.ctx.lineTo(this.location.x, this.location.y);
-                this.ctx.moveTo(400, this.location.y);
-                this.ctx.lineTo(400, 0);
-                this.ctx.stroke();
+                // move the rotation point to the center of the rect
+                this.ctx.translate(this.location.x + width / 2, this.location.y + height / 2);
+                // rotate the rect
+                this.ctx.rotate(this.radians);
+
+                // draw the rect on the transformed context
+                // Note: after transforming [0,0] is visually [x,y]
+                //       so the rect needs to be offset accordingly when drawn
+                this.ctx.rect(-width / 2, -height / 2, width, height);
+
+                this.ctx.fillStyle = "#01a209";
+                this.ctx.fill();
+
+                // restore the context to its untranslated/unrotated state
+                this.ctx.restore();
+
             },
             drawPlayer() {
-                // drawing the player icon onto the canvas
-                this.ctx.setTransform(0.4, 0, 0, 0.4, this.location.x, this.location.y);
-                this.ctx.rotate((Math.PI * this.angle) / 180);
-                this.ctx.drawImage(this.image, -this.image.width / 2, -this.image.height / 2);
+                this.ctx.beginPath();
+                this.ctx.moveTo(400, 820);
+                this.ctx.lineTo(700, 650);
+                this.ctx.lineTo(100, 650);
+                this.ctx.closePath();
+                this.ctx.fillStyle = this.grad;
+                this.ctx.fill();
             },
             clearBoard() {
-                this.ctx.setTransform(1, 0, 0, 1, 0, 0); // setting origin to 0,0
                 this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // clearing the canvas
             }
         },
@@ -72,18 +79,18 @@
             const ctx = canvas.getContext("2d"); // getting the canvas context
             this.canvas = canvas; // setting the canvas as a vue variable
             this.ctx = ctx; // setting the canvas context as a vue variable
-            const image = new Image; // creating a new image
-            image.src = require('../../assets/robotIcon.svg'); // setting image src to be the player icon
-            this.image = image; // setting the image as a vue variable
+            this.grad = this.ctx.createLinearGradient(400, 800, 400, 600);
+            this.grad.addColorStop(0, "rgba(1,92,9,0.44)");
+            this.grad.addColorStop(0.7, "#2b2b2b");
+
             // setting canvas context values for drawing
 
-            this.ctx.lineWidth = 1;
+
             this.ctx.font = "26px Arial";
             this.ctx.strokeStyle = "whitesmoke";
-            this.ctx.setLineDash([12, 30]);
 
             this.$nextTick(function () {
-                this.drawTarget();
+                this.drawPlayer();
             });
         }
     }
