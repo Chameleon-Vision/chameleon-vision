cf981f894aa264d35d87d10b499e7d9da13eca64
cttdev <cttewari@gmail.com>
2020-03-28 17:52:31 -0700

    Applying spotless.

--- chameleon-server/src/main/java/com/chameleonvision/common/vision/pipeline/CVPipe.java
+++ chameleon-server/src/main/java/com/chameleonvision/common/vision/pipeline/CVPipe.java
@@ -3,8 +3,8 @@ package com.chameleonvision.common.vision.pipeline;
 import java.util.function.Function;
 
 /**
- * Defines a pipe. A pipe is a single step in a pipeline.
- * This class is to be extended, never used on its own.
+ * Defines a pipe. A pipe is a single step in a pipeline. This class is to be extended, never used
+ * on its own.
  *
  * @param <I> Input type for the pipe
  * @param <O> Output type for the pipe
@@ -12,32 +12,30 @@ import java.util.function.Function;
  */
 public abstract class CVPipe<I, O, P> implements Function<I, PipeResult<O>> {
 
-    protected PipeResult<O> result = new PipeResult<>();
-    protected P params;
+  protected PipeResult<O> result = new PipeResult<>();
+  protected P params;
 
-    public void setParams(P params) {
-        this.params = params;
-    }
+  public void setParams(P params) {
+    this.params = params;
+  }
 
-    /**
-     * Runs the process for the pipe.
-     *
-     * @param in Input for pipe processing
-     * @return Result of processing
-     */
-    protected abstract O process(I in);
+  /**
+   * Runs the process for the pipe.
+   *
+   * @param in Input for pipe processing
+   * @return Result of processing
+   */
+  protected abstract O process(I in);
 
-    /**
-     *
-     *
-     * @param in Input for pipe processing
-     * @return Result of processing
-     */
-    @Override
-    public PipeResult<O> apply(I in) {
-        long pipeStartNanos = System.nanoTime();
-        result.result = process(in);
-        result.nanosElapsed = System.nanoTime() - pipeStartNanos;
-        return result;
-    }
+  /**
+   * @param in Input for pipe processing
+   * @return Result of processing
+   */
+  @Override
+  public PipeResult<O> apply(I in) {
+    long pipeStartNanos = System.nanoTime();
+    result.result = process(in);
+    result.nanosElapsed = System.nanoTime() - pipeStartNanos;
+    return result;
+  }
 }
