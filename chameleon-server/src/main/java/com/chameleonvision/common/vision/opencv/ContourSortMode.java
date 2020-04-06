0376bdbdcb6bb9e468d353fff24d4c2ac1a0684a
ori agranat <oriagranat9@gmail.com>
2020-04-06 11:48:36 +0300

    updated Largest ContourSortMode and added centermost

--- chameleon-server/src/main/java/com/chameleonvision/common/vision/opencv/ContourSortMode.java
+++ chameleon-server/src/main/java/com/chameleonvision/common/vision/opencv/ContourSortMode.java
@@ -1,18 +1,18 @@
 package com.chameleonvision.common.vision.opencv;
 
 import com.chameleonvision.common.vision.target.TrackedTarget;
+import org.apache.commons.math3.util.FastMath;
+
 import java.util.Comparator;
 
 public enum ContourSortMode {
-    Largest(
-            (rect1, rect2) ->
-                    Double.compare(rect2.getMinAreaRect().size.area(), rect1.getMinAreaRect().size.area())),
+    Largest(Comparator.comparingDouble(TrackedTarget::getArea)),
     Smallest(Largest.getComparator().reversed()),
     Highest(Comparator.comparingDouble(rect -> rect.getMinAreaRect().center.y)),
     Lowest(Highest.getComparator().reversed()),
     Leftmost(Comparator.comparingDouble(target -> target.getMinAreaRect().center.x)),
     Rightmost(Leftmost.getComparator().reversed()),
-    Centermost(null);
+    Centermost(Comparator.comparingDouble(rect -> (FastMath.pow(rect.getMinAreaRect().center.y, 2) + FastMath.pow(rect.getMinAreaRect().center.x, 2))));
 
     private Comparator<TrackedTarget> m_comparator;
 
