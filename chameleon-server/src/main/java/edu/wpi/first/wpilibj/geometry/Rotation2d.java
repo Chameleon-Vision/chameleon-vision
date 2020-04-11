82684920224e5f5ba2924106cbe09beb3d30d42e
Matt <matthew.morley.ca@gmail.com>
2020-04-11 15:46:20 -0700

    Update geometry classes

--- chameleon-server/src/main/java/edu/wpi/first/wpilibj/geometry/Rotation2d.java
+++ chameleon-server/src/main/java/edu/wpi/first/wpilibj/geometry/Rotation2d.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,28 +7,27 @@
 
 package edu.wpi.first.wpilibj.geometry;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import java.io.IOException;
 import java.util.Objects;
 
-/** A rotation in a 2d coordinate frame represented a point on the unit circle (cosine and sine). */
-@JsonSerialize(using = Rotation2d.RotationSerializer.class)
-@JsonDeserialize(using = Rotation2d.RotationDeserializer.class)
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * A rotation in a 2d coordinate frame represented a point on the unit circle
+ * (cosine and sine).
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE)
 public class Rotation2d {
     private final double m_value;
     private final double m_cos;
     private final double m_sin;
 
-    /** Constructs a Rotation2d with a default angle of 0 degrees. */
+    /**
+     * Constructs a Rotation2d with a default angle of 0 degrees.
+     */
     public Rotation2d() {
         m_value = 0.0;
         m_cos = 1.0;
@@ -36,22 +35,25 @@ public class Rotation2d {
     }
 
     /**
-    * Constructs a Rotation2d with the given radian value. The x and y don't have to be normalized.
-    *
-    * @param value The value of the angle in radians.
-    */
-    public Rotation2d(double value) {
+     * Constructs a Rotation2d with the given radian value.
+     * The x and y don't have to be normalized.
+     *
+     * @param value The value of the angle in radians.
+     */
+    @JsonCreator
+    public Rotation2d(@JsonProperty(required = true, value = "radians") double value) {
         m_value = value;
         m_cos = Math.cos(value);
         m_sin = Math.sin(value);
     }
 
     /**
-    * Constructs a Rotation2d with the given x and y (cosine and sine) components.
-    *
-    * @param x The x component or cosine of the rotation.
-    * @param y The y component or sine of the rotation.
-    */
+     * Constructs a Rotation2d with the given x and y (cosine and sine)
+     * components.
+     *
+     * @param x The x component or cosine of the rotation.
+     * @param y The y component or sine of the rotation.
+     */
     @SuppressWarnings("ParameterName")
     public Rotation2d(double x, double y) {
         double magnitude = Math.hypot(x, y);
@@ -66,114 +68,123 @@ public class Rotation2d {
     }
 
     /**
-    * Constructs and returns a Rotation2d with the given degree value.
-    *
-    * @param degrees The value of the angle in degrees.
-    * @return The rotation object with the desired angle value.
-    */
+     * Constructs and returns a Rotation2d with the given degree value.
+     *
+     * @param degrees The value of the angle in degrees.
+     * @return The rotation object with the desired angle value.
+     */
     public static Rotation2d fromDegrees(double degrees) {
         return new Rotation2d(Math.toRadians(degrees));
     }
 
     /**
-    * Adds two rotations together, with the result being bounded between -pi and pi.
-    *
-    * <p>For example, Rotation2d.fromDegrees(30) + Rotation2d.fromDegrees(60) = Rotation2d{-pi/2}
-    *
-    * @param other The rotation to add.
-    * @return The sum of the two rotations.
-    */
+     * Adds two rotations together, with the result being bounded between -pi and
+     * pi.
+     *
+     * <p>For example, Rotation2d.fromDegrees(30) + Rotation2d.fromDegrees(60) =
+     * Rotation2d{-pi/2}
+     *
+     * @param other The rotation to add.
+     * @return The sum of the two rotations.
+     */
     public Rotation2d plus(Rotation2d other) {
         return rotateBy(other);
     }
 
     /**
-    * Subtracts the new rotation from the current rotation and returns the new rotation.
-    *
-    * <p>For example, Rotation2d.fromDegrees(10) - Rotation2d.fromDegrees(100) = Rotation2d{-pi/2}
-    *
-    * @param other The rotation to subtract.
-    * @return The difference between the two rotations.
-    */
+     * Subtracts the new rotation from the current rotation and returns the new
+     * rotation.
+     *
+     * <p>For example, Rotation2d.fromDegrees(10) - Rotation2d.fromDegrees(100) =
+     * Rotation2d{-pi/2}
+     *
+     * @param other The rotation to subtract.
+     * @return The difference between the two rotations.
+     */
     public Rotation2d minus(Rotation2d other) {
         return rotateBy(other.unaryMinus());
     }
 
     /**
-    * Takes the inverse of the current rotation. This is simply the negative of the current angular
-    * value.
-    *
-    * @return The inverse of the current rotation.
-    */
+     * Takes the inverse of the current rotation. This is simply the negative of
+     * the current angular value.
+     *
+     * @return The inverse of the current rotation.
+     */
     public Rotation2d unaryMinus() {
         return new Rotation2d(-m_value);
     }
 
     /**
-    * Multiplies the current rotation by a scalar.
-    *
-    * @param scalar The scalar.
-    * @return The new scaled Rotation2d.
-    */
+     * Multiplies the current rotation by a scalar.
+     *
+     * @param scalar The scalar.
+     * @return The new scaled Rotation2d.
+     */
     public Rotation2d times(double scalar) {
         return new Rotation2d(m_value * scalar);
     }
 
     /**
-    * Adds the new rotation to the current rotation using a rotation matrix.
-    *
-    * <p>The matrix multiplication is as follows: [cos_new] [other.cos, -other.sin][cos] [sin_new] =
-    * [other.sin, other.cos][sin] value_new = atan2(cos_new, sin_new)
-    *
-    * @param other The rotation to rotate by.
-    * @return The new rotated Rotation2d.
-    */
+     * Adds the new rotation to the current rotation using a rotation matrix.
+     *
+     * <p>The matrix multiplication is as follows:
+     * [cos_new]   [other.cos, -other.sin][cos]
+     * [sin_new] = [other.sin,  other.cos][sin]
+     * value_new = atan2(cos_new, sin_new)
+     *
+     * @param other The rotation to rotate by.
+     * @return The new rotated Rotation2d.
+     */
     public Rotation2d rotateBy(Rotation2d other) {
         return new Rotation2d(
-                m_cos * other.m_cos - m_sin * other.m_sin, m_cos * other.m_sin + m_sin * other.m_cos);
+                m_cos * other.m_cos - m_sin * other.m_sin,
+                m_cos * other.m_sin + m_sin * other.m_cos
+        );
     }
 
-    /*
-    * Returns the radian value of the rotation.
-    *
-    * @return The radian value of the rotation.
-    */
+    /**
+     * Returns the radian value of the rotation.
+     *
+     * @return The radian value of the rotation.
+     */
+    @JsonProperty
     public double getRadians() {
         return m_value;
     }
 
     /**
-    * Returns the degree value of the rotation.
-    *
-    * @return The degree value of the rotation.
-    */
+     * Returns the degree value of the rotation.
+     *
+     * @return The degree value of the rotation.
+     */
     public double getDegrees() {
         return Math.toDegrees(m_value);
     }
 
     /**
-    * Returns the cosine of the rotation.
-    *
-    * @return The cosine of the rotation.
-    */
+     * Returns the cosine of the rotation.
+     *
+     * @return The cosine of the rotation.
+     */
     public double getCos() {
         return m_cos;
     }
 
     /**
-    * Returns the sine of the rotation.
-    *
-    * @return The sine of the rotation.
-    */
+     * Returns the sine of the rotation.
+     *
+     * @return The sine of the rotation.
+     */
     public double getSin() {
         return m_sin;
     }
 
     /**
-    * Returns the tangent of the rotation.
-    *
-    * @return The tangent of the rotation.
-    */
+     * Returns the tangent of the rotation.
+     *
+     * @return The tangent of the rotation.
+     */
     public double getTan() {
         return m_sin / m_cos;
     }
@@ -184,11 +195,11 @@ public class Rotation2d {
     }
 
     /**
-    * Checks equality between this Rotation2d and another object.
-    *
-    * @param obj The other object.
-    * @return Whether the two objects are equal or not.
-    */
+     * Checks equality between this Rotation2d and another object.
+     *
+     * @param obj The other object.
+     * @return Whether the two objects are equal or not.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Rotation2d) {
@@ -200,35 +211,5 @@ public class Rotation2d {
     @Override
     public int hashCode() {
         return Objects.hash(m_value);
-    }
-
-    static class RotationSerializer extends StdSerializer<Rotation2d> {
-        RotationSerializer() {
-            super(Rotation2d.class);
-        }
-
-        @Override
-        public void serialize(Rotation2d value, JsonGenerator jgen, SerializerProvider provider)
-                throws IOException, JsonProcessingException {
-
-            jgen.writeStartObject();
-            jgen.writeNumberField("radians", value.m_value);
-            jgen.writeEndObject();
-        }
-    }
-
-    static class RotationDeserializer extends StdDeserializer<Rotation2d> {
-        RotationDeserializer() {
-            super(Rotation2d.class);
-        }
-
-        @Override
-        public Rotation2d deserialize(JsonParser jp, DeserializationContext ctxt)
-                throws IOException, JsonProcessingException {
-            JsonNode node = jp.getCodec().readTree(jp);
-            double radians = node.get("radians").numberValue().doubleValue();
-
-            return new Rotation2d(radians);
-        }
     }
 }
--- chameleon-server/src/test/java/com/chameleonvision/common/vision/pipeline/SolvePNPTest.java
+++ chameleon-server/src/test/java/com/chameleonvision/common/vision/pipeline/SolvePNPTest.java
@@ -1,19 +1,46 @@
 package com.chameleonvision.common.vision.pipeline;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 import com.chameleonvision.common.util.TestUtils;
+import com.chameleonvision.common.util.numbers.DoubleCouple;
+import com.chameleonvision.common.vision.camera.CameraCalibrationCoefficients;
 import com.chameleonvision.common.vision.frame.Frame;
 import com.chameleonvision.common.vision.frame.provider.FileFrameProvider;
 import com.chameleonvision.common.vision.opencv.CVMat;
 import com.chameleonvision.common.vision.opencv.ContourGroupingMode;
 import com.chameleonvision.common.vision.opencv.ContourIntersectionDirection;
-import com.chameleonvision.common.vision.pipe.impl.Draw3dTargetsPipe;
-import org.junit.jupiter.api.Assertions;
+import com.chameleonvision.common.vision.target.TargetModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.event.Level;
 
-public class CornerDetectionTest {
+public class SolvePNPTest {
+
+    @Test
+    public void meme() throws IOException {
+        TestUtils.loadLibraries();
+
+        var lowres = (Path.of(TestUtils.getCalibrationPath().toString(), "lifecamcal.json")
+                .toFile());
+        var cal1 = new ObjectMapper().readValue(lowres, CameraCalibrationCoefficients.class);
+
+        var highres = (Path.of(TestUtils.getCalibrationPath().toString(), "lifecamcal2.json")
+                .toFile());
+        var cal2 = new ObjectMapper().readValue(highres, CameraCalibrationCoefficients.class);
+
+    }
+
+    private CameraCalibrationCoefficients get640p() {
+        try {
+            return new ObjectMapper().readValue((Path.of(TestUtils.getCalibrationPath().toString(), "lifecam640p.json")
+                    .toFile()), CameraCalibrationCoefficients.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     @Test
     public void test2019() {
@@ -36,18 +63,12 @@ public class CornerDetectionTest {
                         TestUtils.getWPIImagePath(TestUtils.WPI2019Image.kCargoStraightDark48in),
                         TestUtils.WPI2019Image.FOV);
 
-//        TestUtils.showImage(frameProvider.getFrame().image.getMat(), "Pipeline input", 1);
-
         CVPipelineResult pipelineResult;
 
         pipelineResult = pipeline.run(frameProvider.getFrame(), settings);
-//        printTestResults(pipelineResult);
-//
-//        Assertions.assertTrue(pipelineResult.hasTargets());
-//        Assertions.assertEquals(2, pipelineResult.targets.size());
 
-
-        TestUtils.showImage(pipelineResult.outputFrame.image.getMat(), "Pipeline output", 1000 * 90);
+        TestUtils.showImage(pipelineResult.outputFrame.image.getMat(), "Pipeline output",
+                1000 * 90);
     }
 
     @Test
@@ -57,22 +78,29 @@ public class CornerDetectionTest {
 
         var settings = new ReflectivePipelineSettings();
         settings.hsvHue.set(60, 100);
-        settings.hsvSaturation.set(200, 255);
-        settings.hsvValue.set(200, 255);
+        settings.hsvSaturation.set(100, 255);
+        settings.hsvValue.set(60, 255);
         settings.outputShowThresholded = true;
+        settings.solvePNPEnabled = true;
+        settings.cornerDetectionUseConvexHulls = true;
+        settings.cameraCalibration = get640p();
+        settings.targetModel = TargetModel.get2020Target();
+        settings.cameraPitch = Rotation2d.fromDegrees(0.0);
+        settings.contourRatio = new DoubleCouple(2.0, 3.0);
 
         var frameProvider =
                 new FileFrameProvider(
-                        TestUtils.getWPIImagePath(TestUtils.WPI2020Image.kBlueGoal_108in_Center),
+                        TestUtils.getWPIImagePath(TestUtils.WPI2020Image.kBlueGoal_Far_ProtectedZone),
                         TestUtils.WPI2020Image.FOV);
 
         CVPipelineResult pipelineResult = pipeline.run(frameProvider.getFrame(), settings);
         printTestResults(pipelineResult);
 
-        TestUtils.showImage(pipelineResult.outputFrame.image.getMat(), "Pipeline output");
+        TestUtils.showImage(pipelineResult.outputFrame.image.getMat(), "Pipeline output", 999999);
     }
 
-    private static void continuouslyRunPipeline(Frame frame, ReflectivePipelineSettings settings) {
+    private static void continuouslyRunPipeline(Frame frame, ReflectivePipelineSettings
+            settings) {
         var pipeline = new ReflectivePipeline();
 
         while (true) {
@@ -109,7 +137,8 @@ public class CornerDetectionTest {
     private static void printTestResults(CVPipelineResult pipelineResult) {
         double fps = 1000 / pipelineResult.getLatencyMillis();
         System.out.print(
-                "Pipeline ran in " + pipelineResult.getLatencyMillis() + "ms (" + fps + " fps), ");
+                "Pipeline ran in " + pipelineResult.getLatencyMillis() + "ms (" + fps + " " +
+                        "fps), ");
         System.out.println("Found " + pipelineResult.targets.size() + " valid targets");
     }
 }
