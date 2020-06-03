f7f28c0e7ff0cee6e55b5fb06d296c382482a164
Banks Troutman <btrout.dhrs@gmail.com>
2020-06-03 02:56:27 -0400

    Split ConfigTest tests in to multiple tests

--- chameleon-server/src/test/java/com/chameleonvision/common/configuration/ConfigTest.java
+++ chameleon-server/src/test/java/com/chameleonvision/common/configuration/ConfigTest.java
@@ -5,41 +5,93 @@ import com.chameleonvision.common.logging.LogGroup;
 import com.chameleonvision.common.logging.Logger;
 import com.chameleonvision.common.util.TestUtils;
 import com.chameleonvision.common.util.file.JacksonUtils;
-import com.chameleonvision.common.vision.pipeline.CVPipelineSettings;
 import com.chameleonvision.common.vision.pipeline.ColoredShapePipelineSettings;
 import com.chameleonvision.common.vision.pipeline.ReflectivePipelineSettings;
 import com.chameleonvision.common.vision.target.TargetModel;
-import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.*;
 
 public class ConfigTest {
 
-    @Test
-    public void testConfig() throws IOException {
+    private static final ConfigManager configMgr = ConfigManager.getInstance();
+    private static final CameraConfiguration cameraConfig = new CameraConfiguration();
+    private static final ReflectivePipelineSettings reflectivePipelineSettings =
+            new ReflectivePipelineSettings();
+    private static final ColoredShapePipelineSettings coloredShapePipelineSettings =
+            new ColoredShapePipelineSettings();
+
+    @BeforeAll
+    public static void init() {
         TestUtils.loadLibraries();
+        Logger.setLevel(LogGroup.General, Level.DE_PEST);
+
+        cameraConfig.name = "TestCamera";
+
+        reflectivePipelineSettings.pipelineNickname = "2019Tape";
+        reflectivePipelineSettings.targetModel = TargetModel.get2019Target();
+
+        coloredShapePipelineSettings.pipelineNickname = "2019Cargo";
+        coloredShapePipelineSettings.pipelineIndex = 1;
+
+        cameraConfig.addPipelineSetting(reflectivePipelineSettings);
+        cameraConfig.addPipelineSetting(coloredShapePipelineSettings);
+    }
+
+    @Test
+    @Order(1)
+    public void serializeConfig() throws IOException {
+        TestUtils.loadLibraries();
+        JacksonUtils.serializer(Path.of("settings.json"), reflectivePipelineSettings);
 
         Logger.setLevel(LogGroup.General, Level.DE_PEST);
-        var config = ConfigManager.getInstance();
-        var camConfig = new CameraConfiguration();
-        camConfig.name = "meme";
+        configMgr.getConfig().addCameraConfig(cameraConfig);
+        configMgr.save();
 
-        camConfig.addPipelineSetting(new ReflectivePipelineSettings());
-        ((ReflectivePipelineSettings) camConfig.pipelineSettings.get(0)).targetModel =
-                TargetModel.get2019Target();
-        camConfig.addPipelineSetting(new ColoredShapePipelineSettings());
-        config.getConfig().addCameraConfig(camConfig);
-        config.save();
-
-        config.load();
+        var camConfDir =
+                new File(
+                        Path.of(ConfigManager.getRootFolder().toString(), "cameras", "TestCamera")
+                                .toAbsolutePath()
+                                .toString());
+        Assertions.assertTrue(camConfDir.exists(), "TestCamera config folder not found!");
 
         Assertions.assertTrue(
-                config.getConfig().getCameraConfigurations().get("meme").pipelineSettings.get(0)
-                        instanceof ReflectivePipelineSettings);
+                Files.exists(Path.of(ConfigManager.getRootFolder().toString(), "hardwareConfig.json")),
+                "hardwareConfig.json file not found!");
         Assertions.assertTrue(
-                config.getConfig().getCameraConfigurations().get("meme").pipelineSettings.get(1)
-                        instanceof ColoredShapePipelineSettings);
+                Files.exists(Path.of(ConfigManager.getRootFolder().toString(), "networkSettings.json")),
+                "networkSettings.json file not found!");
+    }
+
+    @Test
+    @Order(2)
+    public void deserializeConfig() {
+        configMgr.load();
+
+        var reflectivePipelineSettings =
+                configMgr.getConfig().getCameraConfigurations().get("TestCamera").pipelineSettings.get(0);
+        var coloredShapePipelineSettings =
+                configMgr.getConfig().getCameraConfigurations().get("TestCamera").pipelineSettings.get(1);
+
+        Assertions.assertTrue(
+                reflectivePipelineSettings instanceof ReflectivePipelineSettings,
+                "Conig loaded pipeline settings for index 0 not of expected type ReflectivePipelineSettings!");
+        Assertions.assertTrue(
+                coloredShapePipelineSettings instanceof ColoredShapePipelineSettings,
+                "Conig loaded pipeline settings for index 1 not of expected type ColoredShapePipelineSettings!");
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        try {
+            Files.deleteIfExists(Paths.get("settings.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new File(ConfigManager.getRootFolder().toAbsolutePath().toString()).delete();
     }
 }
