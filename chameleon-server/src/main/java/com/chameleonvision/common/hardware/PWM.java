0e3bfa54283ed037508e718988fcad5e3eaaaedc
Neel <neeljos24@gmail.com>
2020-06-15 16:49:59 -0400

    Added GPIO and PWN

---
+++ chameleon-server/src/main/java/com/chameleonvision/common/hardware/PWM.java
@@ -0,0 +1,43 @@
+package com.chameleonvision.common.hardware;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.util.CommandArgumentParser;
+import com.pi4j.wiringpi.Gpio;
+
+public class PWM {
+    private static final GpioController gpio = GpioFactory.getInstance();
+    private final GpioPinPwmOutput pwm;
+
+    public PWM(int address){
+        this.pwm = gpio.provisionPwmOutputPin(CommandArgumentParser.getPin(
+                RaspiPin.class,
+                RaspiPin.getPinByAddress(address)
+        ));
+    }
+
+    public PWM(String name){
+        this.pwm = gpio.provisionPwmOutputPin(CommandArgumentParser.getPin(
+                RaspiPin.class,
+                RaspiPin.getPinByName(name)
+        ));
+    }
+
+    public void setPwmRate(int rate){
+        pwm.setPwm(rate);
+    }
+
+    public void setPwmRange(int range){
+        pwm.setPwmRange(range);
+    }
+
+    public int getPwmRate(){
+        return pwm.getPwm();
+    }
+
+    public static boolean shutdown(){
+        gpio.shutdown();
+        return gpio.isShutdown();
+    }
+
+
+}
