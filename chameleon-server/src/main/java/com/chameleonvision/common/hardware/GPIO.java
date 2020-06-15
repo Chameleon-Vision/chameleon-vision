0cf2c5782d5f493e8955566df5e20d4d7b7939fa
Neel <neeljos24@gmail.com>
2020-06-15 16:50:23 -0400

    spotless

--- chameleon-server/src/main/java/com/chameleonvision/common/hardware/GPIO.java
+++ chameleon-server/src/main/java/com/chameleonvision/common/hardware/GPIO.java
@@ -1,6 +1,5 @@
 package com.chameleonvision.common.hardware;
 
-
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
@@ -10,41 +9,40 @@ public class GPIO {
     private static final GpioController gpio = GpioFactory.getInstance();
     private final GpioPinDigitalOutput pin;
 
-    public GPIO(int address){
+    public GPIO(int address) {
         this.pin = gpio.provisionDigitalOutputPin(RaspiPin.getPinByAddress(address));
     }
 
-    public GPIO(String name){
+    public GPIO(String name) {
         this.pin = gpio.provisionDigitalOutputPin(RaspiPin.getPinByName(name));
     }
 
-    public void togglePin(){
+    public void togglePin() {
         pin.toggle();
     }
 
-    public void setLow(){
+    public void setLow() {
         pin.low();
     }
 
-    public void setHigh(){
+    public void setHigh() {
         pin.high();
     }
 
-    public void setState(boolean state){
+    public void setState(boolean state) {
         pin.setState(state);
     }
 
-    public void blink(long delay, long duration){
+    public void blink(long delay, long duration) {
         pin.blink(delay, duration);
     }
 
-    public void pulse(long duration, boolean blocking){
+    public void pulse(long duration, boolean blocking) {
         pin.pulse(duration, blocking);
     }
 
-    public static boolean shutdown(){
+    public static boolean shutdown() {
         gpio.shutdown();
         return gpio.isShutdown();
     }
-
 }
