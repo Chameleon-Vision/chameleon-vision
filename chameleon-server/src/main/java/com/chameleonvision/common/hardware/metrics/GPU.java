98c85c86cf86cdf810b1dd83cee27efe45672326
Neel <neeljos24@gmail.com>
2020-06-21 13:36:25 -0400

    Fixed GPU commands

--- chameleon-server/src/main/java/com/chameleonvision/common/hardware/metrics/GPU.java
+++ chameleon-server/src/main/java/com/chameleonvision/common/hardware/metrics/GPU.java
@@ -2,7 +2,7 @@ package com.chameleonvision.common.hardware.metrics;
 
 
 public class GPU extends MetricsBase{
-    private static final String memoryCommand = "vcgencmd get_mem gpu | grep -x -E '[0-9]+'";
+    private static final String memoryCommand = "vcgencmd get_mem gpu | grep -Eo '[0-9]+'";
     private static final String temperatureCommand = "vcgencmd measure_temp | sed 's/[^0-9]*//g'\n";
 
     public static double getMemory() {
