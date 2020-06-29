16e831228047fbda01487bb0de22d528e40692ec
Unknown <oriagranat9@gmail.com>
2020-06-29 13:20:15 +0300

    spotless

--- chameleon-server/src/main/java/com/chameleonvision/common/dataflow/providers/UIProvider.java
+++ chameleon-server/src/main/java/com/chameleonvision/common/dataflow/providers/UIProvider.java
@@ -1,11 +1,9 @@
 package com.chameleonvision.common.dataflow.providers;
 
 import com.chameleonvision.common.configuration.ConfigManager;
-import com.chameleonvision.common.util.numbers.DoubleCouple;
 import com.chameleonvision.common.vision.frame.FrameDivisor;
 import com.chameleonvision.common.vision.pipeline.ReflectivePipeline;
 import com.chameleonvision.common.vision.processes.VisionModule;
-
 import java.lang.reflect.Field;
 
 public class UIProvider extends Provider {
@@ -15,58 +13,70 @@ public class UIProvider extends Provider {
 
     public void onMessage(String key, Object value) {
         switch (key) {
-            case "changePipelineName": {
-                parentModule.pipelineManager.getCurrentPipeline().getSettings().pipelineNickname =
-                        (String) value;
-                break;
-            }
-            case "addNewPipeline": {
-                ReflectivePipeline pipe = new ReflectivePipeline();
-                pipe.getSettings().pipelineNickname = (String) value;
-                parentModule.pipelineManager.addPipeline(pipe);
-                break;
-            }
-            case "currentPipeline": {
-                parentModule.pipelineManager.changeCurrentPipeline((Integer) value);
-                break;
-            }
-            case "command": {
-                switch ((String) value) {
-                    case "deleteCurrentPipeline":
-                        parentModule.pipelineManager.removeCurrentPipeline();
-                        break;
-                    case "save":
-                        ConfigManager.getInstance().save();
-                        break;
+            case "changePipelineName":
+                {
+                    parentModule.pipelineManager.getCurrentPipeline().getSettings().pipelineNickname =
+                            (String) value;
+                    break;
                 }
-            }
-            default: {
-                setField(parentModule.pipelineManager.getCurrentPipeline().getSettings(), key, value);
-                break;
-            }
+            case "addNewPipeline":
+                {
+                    ReflectivePipeline pipe = new ReflectivePipeline();
+                    pipe.getSettings().pipelineNickname = (String) value;
+                    parentModule.pipelineManager.addPipeline(pipe);
+                    break;
+                }
+            case "currentPipeline":
+                {
+                    parentModule.pipelineManager.changeCurrentPipeline((Integer) value);
+                    break;
+                }
+            case "command":
+                {
+                    switch ((String) value) {
+                        case "deleteCurrentPipeline":
+                            parentModule.pipelineManager.removeCurrentPipeline();
+                            break;
+                        case "save":
+                            ConfigManager.getInstance().save();
+                            break;
+                    }
+                }
+            default:
+                {
+                    setField(parentModule.pipelineManager.getCurrentPipeline().getSettings(), key, value);
+                    break;
+                }
         }
         switch (key) {
-            case "exposure": {
-                parentModule.getVisionSource().getSettables().setExposure((Integer) value);
-                break;
-            }
-            case "brightness": {
-                parentModule.getVisionSource().getSettables().setBrightness((Integer) value);
-                break;
-            }
-            case "gain": {
-                parentModule.getVisionSource().getSettables().setGain((Integer) value);
-                break;
-            }
-            case "videoModeIndex": {
-                var videoMode = parentModule.getVisionSource().getSettables().getAllVideoModes().get((Integer) value);
-                parentModule.getVisionSource().getSettables().setCurrentVideoMode(videoMode);
-                break;
-            }
-            case "streamDivisor": {
-                parentModule.pipelineManager.getCurrentPipeline().getSettings().outputFrameDivisor = FrameDivisor.values()[(Integer) value];
-                break;
-            }
+            case "exposure":
+                {
+                    parentModule.getVisionSource().getSettables().setExposure((Integer) value);
+                    break;
+                }
+            case "brightness":
+                {
+                    parentModule.getVisionSource().getSettables().setBrightness((Integer) value);
+                    break;
+                }
+            case "gain":
+                {
+                    parentModule.getVisionSource().getSettables().setGain((Integer) value);
+                    break;
+                }
+            case "videoModeIndex":
+                {
+                    var videoMode =
+                            parentModule.getVisionSource().getSettables().getAllVideoModes().get((Integer) value);
+                    parentModule.getVisionSource().getSettables().setCurrentVideoMode(videoMode);
+                    break;
+                }
+            case "streamDivisor":
+                {
+                    parentModule.pipelineManager.getCurrentPipeline().getSettings().outputFrameDivisor =
+                            FrameDivisor.values()[(Integer) value];
+                    break;
+                }
         }
     }
 
